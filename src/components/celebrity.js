dc6183dff3fdaf79d5a1d7f6f9432372a1ee8026
geallen <gamze.sen3827@gmail.com>
2020-05-02 15:46:56 +0300

    state with clik event

--- src/components/celebrity.js
+++ src/components/celebrity.js
@@ -8,20 +8,41 @@ export default class celebrity extends React.Component {
         movie : "NA",
         age : "NA"
     }
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isVisible : false
+        }
+
+        this.onClickName = this.onClickName.bind(this);
+    }
  
+    onClickName(e){
+        this.setState({ isVisible : true})
+    }
+
     render(){
     const {name, movie, age} = this.props;
+
+    const { isVisible } = this.state;
     return (
         <div className="col-md-8 mb-4">
             <div className="card">
                 <div className="card-header d-flex justify-content-between">
-                    <h4 className="d-inline">{name}</h4>
+                    <h4 className="d-inline" onClick= {this.onClickName}>{name}</h4>
                     <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
                 </div>
+
+                { isVisible ? 
+                
                 <div className="card-body">
                     <p className="card-text">Movie : {movie}</p>
                     <p className="card-text">Age : {age}</p>
                 </div>
+                : null}
+                
             </div>             
         </div>
     )
